70dd6eb54c85b0157bb24fdac76a66cccad3f594
David Kowis <dkowis@shlrm.org>
2014-05-14 21:22:21 -0500

    Fixing it! yay!

--- spec/my_example_spec.rb
+++ spec/my_example_spec.rb
@@ -2,6 +2,6 @@ require 'my_example'
 
 describe MyExample do
     it 'returns "Hello World"' do
-        expect(MyExample.new.win).to eq("Hello world")
+        expect(MyExample.new.win).to eq("Hello World")
     end
 end
